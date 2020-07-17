2b984620335d983c2b624041df7e95aa7fdd1ced
大陈子 <1379940381@qq.com>
2020-07-18 00:23:01 +0800

    测试

---
+++ .vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+        ],
+        sidebar: {
+            '/foo/': [
+                '',     /* /foo/ */
+                'one',  /* /foo/one.html */
+                'two'   /* /foo/two.html */
+            ],
+
+            '/bar/': [
+                '',      /* /bar/ */
+                'three', /* /bar/three.html */
+                'four'   /* /bar/four.html */
+            ],
+
+            // fallback
+            '/': [
+                '',        /* / */
+                'contact', /* /contact.html */
+                'about'    /* /about.html */
+            ]
+        }
+    }
+}
